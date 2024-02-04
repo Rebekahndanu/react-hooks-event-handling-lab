c0e570b5d6445d59a9e4605b784b6b117dd042af
Rebekah Ndanu <rebekah.ndanu@student.moringaschool.com>
2024-02-04 07:59:42 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+import React from 'react';
+
+function EyesOnMe() {
+    function registerFocus() {
+        console.log('Good!');
+    }
+
+    function registerBlur() {
+        console.log('Hey! Eyes on me!');
+    }
+
+    return (
+        <button onFocus={registerFocus} onBlur={registerBlur}>
+            Eyes on me
+        </button>
+    );
+}
+
+export default EyesOnMe;
